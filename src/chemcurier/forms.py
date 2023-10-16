a3f55db68fef04587f61095670a70ce6d4137da4
Your Name <club11@bk.ru>
2023-10-17 00:04:45 +0300

    CHEMCOURIER IMPROVEMENTS 0.7.3 at 16/10/2023

--- src/chemcurier/forms.py
+++ src/chemcurier/forms.py
@@ -10,18 +10,25 @@ PERIODS = None
 INITIAL_TYREISIZE = None
 TYRESIZES = None
 
+INITIAL_BRANDS = None
+BRANDS = None
+
+INITIAL_RECIEVER = None
+RECIEVERS = None 
+
+INITIAL_PRODCOUTRYS = None
+PRODCOUTRYS = None
+
 def get_chem_periods():
     earlest_date = prices_models.ChemCurierTyresModel.objects.earliest('data_month_chem').data_month_chem
     latestst_date = prices_models.ChemCurierTyresModel.objects.latest('data_month_chem').data_month_chem
     period_of_dates_chem_in_base = pd.date_range(earlest_date, latestst_date, freq='MS').date
-
     list_of_choices = []
     for dattte in period_of_dates_chem_in_base:
         str_date = dattte.strftime('%m.%Y')
         str_date_and_date = dattte, str_date
         list_of_choices.append(str_date_and_date)
     list_of_choices = list(reversed(list_of_choices))
-    print('list_of_choices!!!!!!', list_of_choices)
     return list_of_choices
 
 def get_tyresizes_list():
@@ -30,17 +37,50 @@ def get_tyresizes_list():
     for obj in prices_models.ChemCurierTyresModel.objects.all():
         list_of_tyresizes_only.append(obj.tyre_size_chem)
     list_of_tyresizes_only = list(set(list_of_tyresizes_only))  
-    print('get_tyresizes!!', list_of_tyresizes_only)
     for trsz in list_of_tyresizes_only:      # добавляем ключи:
         resizes_k_and_val = trsz, trsz
         list_of_tyresizes.append(resizes_k_and_val)
     return list_of_tyresizes
 
+def get_tyrebrands_list():
+    list_of_tyrebrandss = [('-','-')]
+    list_of_tyrebrands_only = [] 
+    for obj in prices_models.ChemCurierTyresModel.objects.all():
+      list_of_tyrebrands_only.append(obj.producer_chem)  
+    list_of_tyrebrands_only = list(set(list_of_tyrebrands_only))
+    for brnd in list_of_tyrebrands_only:      # добавляем ключи:
+        tyrebrands_val = brnd, brnd
+        list_of_tyrebrandss.append(tyrebrands_val)
+    return list_of_tyrebrandss  
+
+def get_recievers_list():
+    list_of_recievers = [('-','-')]
+    list_of_recievers_only = [] 
+    for obj in prices_models.ChemCurierTyresModel.objects.all():
+      list_of_recievers_only.append(obj.reciever_chem)  
+    list_of_recievers_only = list(set(list_of_recievers_only))
+    for reciever_chem in list_of_recievers_only:      # добавляем ключи:
+        recievers_val = reciever_chem, reciever_chem
+        list_of_recievers.append(recievers_val)
+    return list_of_recievers  
+
+def get_prod_countrys_list():
+    list_of_prod_country = [('-','-')]
+    list_of_prod_country_only = [] 
+    for obj in prices_models.ChemCurierTyresModel.objects.all():
+      list_of_prod_country_only.append(obj.prod_country)  
+    list_of_prod_country_only = list(set(list_of_prod_country_only))
+    for prod_country in list_of_prod_country_only:      # добавляем ключи:
+        prod_country_val = prod_country, prod_country
+        list_of_prod_country.append(prod_country_val)
+    return list_of_prod_country  
 
 
 PERIODS = get_chem_periods() 
 TYRESIZES = get_tyresizes_list()
-
+BRANDS = get_tyrebrands_list()
+RECIEVERS = get_recievers_list()
+PRODCOUTRYS = get_prod_countrys_list()
 
 class PeriodForm(forms.Form): 
     Parameter_CHOICES = PERIODS   
@@ -57,4 +97,31 @@ class TyreSizeForm(forms.Form):
         widget = forms.Select,
         choices = Parameter_CHOICES_TYRESIZES,
         label='Типоразмер',     
+    )
+
+class BrandForm(forms.Form): 
+    Parameter_CHOICES_BRANDS = BRANDS  
+    tyrebrands = forms.ChoiceField(
+        widget = forms.Select,
+        choices = Parameter_CHOICES_BRANDS,
+        label='Бренд',   
+        required=False,  
+    )
+
+class RecieverForm(forms.Form): 
+    Parameter_CHOICES_RECIEVERS = RECIEVERS  
+    recievers = forms.ChoiceField(
+        widget = forms.Select,
+        choices = Parameter_CHOICES_RECIEVERS,
+        label='Получатель',   
+        required=False,  
+    )
+
+class ProdCountryForm(forms.Form): 
+    Parameter_CHOICES_PRODCOUTRYS = PRODCOUTRYS  
+    prod_countrys = forms.ChoiceField(
+        widget = forms.Select,
+        choices = Parameter_CHOICES_PRODCOUTRYS,
+        label='Стана производства',   
+        required=False,  
     )
--- src/chemcurier/views.py
+++ src/chemcurier/views.py
@@ -43,22 +43,59 @@ class ChemcourierTableModelDetailView(DetailView):
         context['period_form'] = period_form
         
         # 2 получение типоразмера для отбора (создание формы):
-        tyresizes_form = forms.TyreSizeForm()
-        context['tyresizes_form'] = tyresizes_form
-
         tyresize_to_check = None
         if forms.INITIAL_TYREISIZE:
+            tyresizes_form = forms.TyreSizeForm(initial={'tyresizes': forms.INITIAL_TYREISIZE})
             tyresize_to_check = forms.INITIAL_TYREISIZE
         else:
             if forms.TYRESIZES:
+                tyresizes_form = forms.TyreSizeForm()
                 tyresize_to_check = forms.TYRESIZES[0]      #берем первый из списка
+        context['tyresizes_form'] = tyresizes_form
+        # 3 (необязательные)
+        # 3.1 получение бренда для отбора (создание формы):
+        tyrebrands_to_check = None
+        if forms.INITIAL_BRANDS and forms.INITIAL_BRANDS != '-':
+            tyrebrands_form = forms.BrandForm(initial={'tyrebrands': forms.INITIAL_BRANDS})
+            tyrebrands_to_check = forms.INITIAL_BRANDS
+        else:    
+            tyrebrands_form = forms.BrandForm()
+        context['tyrebrands_form'] = tyrebrands_form
 
-        # 3 получить все объекты химкурьер за исключением нулевых значений (шт. деньги) на дату:
+        # 3.2 получение получателя для отбора (создание формы):
+        recievers_to_check = None
+        if forms.INITIAL_RECIEVER and forms.INITIAL_RECIEVER != '-':
+            recievers_form = forms.RecieverForm(initial={'recievers': forms.INITIAL_RECIEVER})
+            recievers_to_check = forms.INITIAL_RECIEVER
+        else:    
+            recievers_form = forms.RecieverForm()
+        context['recievers_form'] = recievers_form      
 
+
+        # 3.3 получение получателя для отбора (создание формы):
+        prod_countrys_to_check = None
+        if forms.INITIAL_PRODCOUTRYS and forms.INITIAL_PRODCOUTRYS != '-':
+            prod_countrys_form = forms.ProdCountryForm(initial={'prod_countrys': forms.INITIAL_PRODCOUTRYS})
+            prod_countrys_to_check = forms.INITIAL_PRODCOUTRYS
+        else:    
+            prod_countrys_form = forms.ProdCountryForm()
+        context['prod_countrys_form'] = prod_countrys_form   
+
+
+        # 4 получить все объекты химкурьер за исключением нулевых значений (шт. деньги) на дату:
         get_chem_courier_objects_from_base = prices_models.ChemCurierTyresModel.objects.filter(data_month_chem__month=chosen_month_num, data_month_chem__year=chosen_year_num, tyre_size_chem=tyresize_to_check).exclude(average_price_in_usd__isnull=True)      
+        # 4.1 доп. проверки:
+        if tyrebrands_to_check:   
+            get_chem_courier_objects_from_base = get_chem_courier_objects_from_base.filter(producer_chem=tyrebrands_to_check)
+        if recievers_to_check:   
+            get_chem_courier_objects_from_base = get_chem_courier_objects_from_base.filter(reciever_chem=recievers_to_check)
+        if prod_countrys_to_check:   
+            get_chem_courier_objects_from_base = get_chem_courier_objects_from_base.filter(prod_country=prod_countrys_to_check)
+
+        
+
+
         get_chem_courier_objects_from_base = get_chem_courier_objects_from_base
-        
-        
         context['get_chem_courier_objects_from_base'] = get_chem_courier_objects_from_base
         return context  
     
@@ -75,5 +112,25 @@ class ChemcourierTableModelUpdateView(View):
         if get_tyresize:
             forms.INITIAL_TYREISIZE = get_tyresize
 
+        # 3. получаем бренд от пользователя для поиска и установки initial значения в бренде формы выбора    
+        get_tyrebrand = request.POST.get('tyrebrands') 
+        if get_tyrebrand and get_tyrebrand != '-':
+            forms.INITIAL_BRANDS = get_tyrebrand
+        else:
+            forms.INITIAL_BRANDS = None
+
+        # 4. получаем получателя от пользователя для поиска и установки initial значения в получателях формы выбора    
+        get_reciever = request.POST.get('recievers') 
+        if get_reciever and get_reciever != '-':
+            forms.INITIAL_RECIEVER = get_reciever
+        else:
+            forms.INITIAL_RECIEVER = None
+
+        # 5. получаем страну производства от пользователя для поиска и установки initial значения в странах производства формы выбора    
+        get_prod_countrys = request.POST.get('prod_countrys') 
+        if get_prod_countrys and get_prod_countrys != '-':
+            forms.INITIAL_PRODCOUTRYS = get_prod_countrys
+        else:
+            forms.INITIAL_PRODCOUTRYS = None
 
         return HttpResponseRedirect(reverse_lazy('chemcurier:chemcurier_table'))
