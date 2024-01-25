bb51ed63d99e6ae4d6d5a2c55c84e586169a2a74
Your Name <club11@bk.ru>
2024-01-26 00:49:00 +0300

    improves webdriver v.0.2 at 26/01/2024

--- src/prices/views.py
+++ src/prices/views.py
@@ -97,14 +97,10 @@ class ComparativeAnalysisTableModelDetailView(DetailView):
             
 
             chromeOptions = webdriver.ChromeOptions() 
-            chromeOptions.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2}) 
+
             chromeOptions.add_argument("--no-sandbox") 
             chromeOptions.add_argument("--disable-setuid-sandbox") 
-            chromeOptions.add_argument("--disable-dev-shm-using") 
-            chromeOptions.add_argument("--disable-extensions") 
-            chromeOptions.add_argument("--disable-gpu") 
-            chromeOptions.add_argument("start-maximized") 
-            chromeOptions.add_argument("disable-infobars") 
+            chromeOptions.add_argument("--disable-dev-shm-usage");
             chromeOptions.add_argument("--headless") 
 
             webdriverr_global = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), chrome_options=chromeOptions)
