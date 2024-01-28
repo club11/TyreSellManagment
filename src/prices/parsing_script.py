d928712c741b4ec6a291cbc70145cb45f4ac71bb
Your Name <club11@bk.ru>
2024-01-28 19:22:52 +0300

    RUNNING PROGRAMM function 1.1.4  in parsing_sript.py at 28/01/2024

--- src/prices/parsing_script.py
+++ src/prices/parsing_script.py
@@ -66,9 +66,9 @@ def belarus_sites_parsing():
 
             # 1 ###### ПАРСИНГ Onliner:    
             chromeOptions1 = webdriver.ChromeOptions() 
-        #    chromeOptions1.add_argument("--no-sandbox") 
-        #    chromeOptions1.add_argument("--disable-setuid-sandbox") 
-        #    chromeOptions1.add_argument("--disable-dev-shm-usage");
+            chromeOptions1.add_argument("--no-sandbox") 
+            chromeOptions1.add_argument("--disable-setuid-sandbox") 
+            chromeOptions1.add_argument("--disable-dev-shm-usage");
         #    chromeOptions1.add_argument("--headless") 
         ###    webdriverr_global = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chromeOptions1)
             webdriverr_global = webdriver.Chrome(options=chromeOptions1)
