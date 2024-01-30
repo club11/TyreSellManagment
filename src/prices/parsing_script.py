6b66304ef9c1c2d6678c35660f0786cbb8ca6365
Your Name <club11@bk.ru>
2024-01-30 23:32:41 +0300

    RUNNING PROGRAMM function 1.40  in parsing_sript.py at 30/01/2024

--- src/prices/parsing_script.py
+++ src/prices/parsing_script.py
@@ -2269,35 +2269,35 @@ def russia_sites_parsing():
             # 3 END  ###### ПАРСИНГ KOLESA_DAROM
 
 def running_programm():
-    ###get_year  = datetime.datetime.now().year
-    ###get_month  = datetime.datetime.now().month
-    ###get_day  = datetime.datetime.now().day
-    ###     
-    ###start = datetime.datetime(get_year, get_month, get_day, 12, 30) # !!!!!!! для введения часа и мин ля запуска скрипта
-    ###delta = datetime.timedelta(minutes=0)
-    ###end = start + delta
-    ###end_hour = end.hour
-    ###end_minute = end.minute
-    ###end_execution = start + datetime.timedelta(minutes=2)
-    ####print('end_hour', end_hour, 'end_minute', end_minute)
-    ###couple_min_checking = datetime.timedelta(minutes=1)
-    ###get_current_time = datetime.datetime.now()
-    ###minutes_to_start_left =  start - get_current_time 
-    ###print('!!!', minutes_to_start_left)
-    ###if minutes_to_start_left < couple_min_checking: # если до времени запуска скрипта осталось менее минуты - то начать
-    ###    #while True:
-    ###    ##    print(datetime.datetime.now())
-    ###    #    current_time = datetime.datetime.now()
-    ###    #    if current_time.hour == end_hour and current_time.minute == end_minute:    
-    ###    #        belarus_sites_parsing()
-    ###    #        break
-    ###    #    elif current_time.hour == end_execution.hour and current_time.minute == end_execution.minute:  
-    ###    #        break
-    ###    belarus_sites_parsing()
+    get_year  = datetime.datetime.now().year
+    get_month  = datetime.datetime.now().month
+    get_day  = datetime.datetime.now().day
+         
+    start = datetime.datetime(get_year, get_month, get_day, 20, 40) # !!!!!!! для введения часа и мин ля запуска скрипта
+    delta = datetime.timedelta(minutes=0)
+    end = start + delta
+    end_hour = end.hour
+    end_minute = end.minute
+    end_execution = start + datetime.timedelta(minutes=2)
+    #print('end_hour', end_hour, 'end_minute', end_minute)
+    couple_min_checking = datetime.timedelta(minutes=1)
+    get_current_time = datetime.datetime.now()
+    minutes_to_start_left =  start - get_current_time 
+    print('!!!', minutes_to_start_left)
+    if minutes_to_start_left < couple_min_checking: # если до времени запуска скрипта осталось менее минуты - то начать
+        #while True:
+        ##    print(datetime.datetime.now())
+        #    current_time = datetime.datetime.now()
+        #    if current_time.hour == end_hour and current_time.minute == end_minute:    
+        #        belarus_sites_parsing()
+        #        break
+        #    elif current_time.hour == end_execution.hour and current_time.minute == end_execution.minute:  
+        #        break
+        belarus_sites_parsing()
 
 
-    belarus_sites_parsing()
-    pass
+    #belarus_sites_parsing()
+    #pass
     return 'the programm is fullfilled'
 
 running_programm()
