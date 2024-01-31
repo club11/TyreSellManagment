63eefc1a3e8033f9b03214912e4a7942de750ee7
Your Name <club11@bk.ru>
2024-01-31 22:15:57 +0300

    RUNNING PROGRAMM function 1.40  in parsing_sript.py at 30/01/2024

--- src/prices/parsing_script.py
+++ src/prices/parsing_script.py
@@ -2309,29 +2309,29 @@ def running_test_programm():
 
     a = requests.get('https://catalog.onliner.by/tires')
 
-    #chromeOptions1 = webdriver.ChromeOptions() 
-    #chromeOptions1.add_argument("--no-sandbox") 
-    #chromeOptions1.add_argument("--disable-setuid-sandbox") 
-    #chromeOptions1.add_argument("--disable-dev-shm-usage");
-    #chromeOptions1.add_argument("--headless") 
-    #webdriverr_global = webdriver.Chrome(options=chromeOptions1)  
-    #url = 'https://catalog.onliner.by/tires'
-    #webdriverr = webdriverr_global
-    #webdriverr.get(url)
-    #time.sleep(2)
-    #webdriverr.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    #time.sleep(5)
-    #soup = BeautifulSoup(webdriverr.page_source,'lxml')
-    #pages = soup.find('ul', class_='catalog-pagination__pages-list')
-    #urls = []
-    ##links = pages.find_all('a', class_='schema-pagination__pages-link') #
-    #links = pages.find_all('a', class_='catalog-pagination__pages-link')
-    #for link in links:
-    #    pageNum = int(link.text) if link.text.isdigit() else None
-    #    if pageNum != None:
-    #        urls.append(pageNum)
-#
-#
+    chromeOptions1 = webdriver.ChromeOptions() 
+    chromeOptions1.add_argument("--no-sandbox") 
+    chromeOptions1.add_argument("--disable-setuid-sandbox") 
+    chromeOptions1.add_argument("--disable-dev-shm-usage");
+    chromeOptions1.add_argument("--headless") 
+    webdriverr_global = webdriver.Chrome(options=chromeOptions1)  
+    url = 'https://catalog.onliner.by/tires'
+    webdriverr = webdriverr_global
+    webdriverr.get(url)
+    time.sleep(2)
+    webdriverr.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(5)
+    soup = BeautifulSoup(webdriverr.page_source,'lxml')
+    pages = soup.find('ul', class_='catalog-pagination__pages-list')
+    urls = []
+    #links = pages.find_all('a', class_='schema-pagination__pages-link') #
+    links = pages.find_all('a', class_='catalog-pagination__pages-link')
+    for link in links:
+        pageNum = int(link.text) if link.text.isdigit() else None
+        if pageNum != None:
+            urls.append(pageNum)
+
+
     return print(' +++++++++++ === =====++++++++the programm is fullfilled1')
 
-#running_test_programm()
+running_test_programm()
