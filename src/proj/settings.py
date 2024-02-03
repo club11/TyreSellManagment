6b50114554518758ffb0d07375426821e357b9f8
Your Name <club11@bk.ru>
2024-02-03 22:58:54 +0300

    RUNNING PROGRAMM function 1.40  in parsing_sript.py at 30/01/2024

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'chemcurier',
     'profiles',
 
-    'crispy_forms',
+    #'crispy_forms',
 
 
 ]
@@ -148,6 +148,6 @@ MEDIA_ROOT = BASE_DIR /'media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CRISPY_TEMPLATE_PACK = 'uni_form'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 LOGIN_REDIRECT_URL = '/chemcurier/chemcurier_progressive'
