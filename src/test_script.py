ceeeacddda4b05787e1aad9605121ce91d33f7fb
Your Name <club11@bk.ru>
2024-02-04 14:51:25 +0300

    RUNNING PROGRAMM function 1.40  in parsing_sript.py at 30/01/2024

--- src/test_script.py
+++ src/test_script.py
@@ -45,6 +45,7 @@ def main():
             from selenium.webdriver.chrome.options import Options
 
             options = Options()
+            options.add_argument("--disable-dev-shm-usage")
             options.add_argument('--headless=new')
             webdriverr_global = webdriver.Chrome(options=options)           
             url = 'https://catalog.onliner.by/tires'
