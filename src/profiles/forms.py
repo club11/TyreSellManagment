d4e1fe3a83fc6991dabe8d93a8ec4c7941898598
Your Name <club11@bk.ru>
2024-10-06 11:30:48 +0300

    synology version v0.64.59.0

--- src/profiles/forms.py
+++ src/profiles/forms.py
@@ -11,15 +11,15 @@ username_validator = UnicodeUsernameValidator()
 
 
 class RegisterForm(forms.Form):
-    username = forms.CharField(max_length=150, required=True, label='Username', validators=[username_validator])
+    username = forms.CharField(max_length=150, required=True, label='Имя пользователя', validators=[username_validator])
     password1 = forms.CharField(
-        label="Password",
+        label="Пароль",
         strip=False,
         widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
         help_text=password_validation.password_validators_help_text_html(),
     )
     password2 = forms.CharField(
-        label="Password confirmation",
+        label="Подтверждение пароля",
         widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
         strip=False,
         help_text="Enter the same password as before, for verification.",
